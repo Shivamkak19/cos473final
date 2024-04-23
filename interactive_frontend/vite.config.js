3670b699a0858baa2ae19dc73d0988270a0a0884
Shivamkak19 <117538014+Shivamkak19@users.noreply.github.com>
2024-04-23 06:59:50 -0400

    config

--- interactive_frontend/vite.config.js
+++ interactive_frontend/vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        bot: './bot.html'
+      },
+    },
+    outDir: 'dist',
+  },
   plugins: [react()],
 })
+
