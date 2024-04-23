fa4bd55ab3d36db0dfdb41d9f8a3c275a9cf0a41
Shivamkak19 <117538014+Shivamkak19@users.noreply.github.com>
2024-04-23 06:59:00 -0400

    reset config

--- interactive_frontend/vite.config.js
+++ interactive_frontend/vite.config.js
@@ -3,15 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: './index.html',
-        bot: './bot.html'
-      },
-    },
-    outDir: 'dist',
-  },
   plugins: [react()],
 })
-
